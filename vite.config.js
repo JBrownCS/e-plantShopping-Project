cd1b201c044a54be836c23f8c054915889636825
JBrownCS <jshnsam@gmail.com>
2025-04-29 18:17:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/plant_nursery_shopping",
+  base: "/client/",
   plugins: [react()],
 })
