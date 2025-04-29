e0ac67a3876433a58adf3adc3604d333d91b1952
Joshua Brown <jshnsam@gmail.com>
2025-04-29 08:18:07 -0400

    Added code for handleContinueShopping function and added handleCheckoutShopping Function

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -19,10 +19,12 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-        
+        onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
   };
