d23f70869fd1efd9a0a97993a2c622b61b64b3a5
Joshua Brown <jshnsam@gmail.com>
2025-04-29 08:09:46 -0400

    Added code for addItem, removeItem, and updateQuantity functions

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,24 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const {pName, pImage, pCost} = state[action.payload];
+        const existingPlant = state.items.find(item => item.name == pName);
+        if(existingPlant){
+            existingPlant.quantity++;
+        }
+        else{
+            state.items.push({pName, pImage, pCost, quantity: 1});
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name != action.payload);
     },
     updateQuantity: (state, action) => {
-
+        const {pName, pQuantity} = state[action.payload];
+        const existingPlant = state.items.find(item => item.name == pName);
+        if(existingPlant){
+            existingPlant.quantity = pQuantity;
+        }
     
     },
   },
